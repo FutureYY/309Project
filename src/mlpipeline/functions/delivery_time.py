c933b99de167b8057323243e5e04074e1ec1c1e2
jiaawennnn <jiawen27.chua@gmail.com>
2025-06-01 11:32:51 +0800

    Added select columns to "delivery_speed_flag" function

--- src/mlpipeline/functions/delivery_time.py
+++ src/mlpipeline/functions/delivery_time.py
@@ -16,16 +16,15 @@ def time_taken_to_deliver(df):
 
 # sortds orders into fast, normal, slow delivery
 # fast delivery = 1, normal dlivery = 2, slow delivery = 3
-def flag_delivery_speed_relative(df, delivery_time_col="delivered_in_days"):
+def flag_delivery_speed_flag(df, delivery_time_col="delivered_in_days"):
   
     avg_val = df.select(avg(col(delivery_time_col)).alias("avg_val")).collect()[0]["avg_val"]
 
-    df_flagged = df.withColumn("avg_delivery_time_days", col(delivery_time_col)) \
-        .withColumn(
+    df_flagged = df.withColumn(
             "delivery_speed_flag",
-            when(col(delivery_time_col) <= avg_val - 1, 1)      # for fast delivery timing
-            .when(col(delivery_time_col) <= avg_val + 1, 2)     # for normal delivery timing
-            .otherwise(3)                                       # for slow delivery timing
-        )
+            when(col(delivery_time_col) <= avg_val - 1, 1)      # Fast
+            .when(col(delivery_time_col) <= avg_val + 1, 2)     # Normal
+            .otherwise(3)                                       # Slow
+        ).select("order_id", "delivered_in_days", "delivery_speed_flag")
 
     return df_flagged
