b2e99119e86b8d1d2e3693bef50b5c136ff1d35b
Yoshana18 <yoshana.magendran.18@gmail.com>
2025-06-08 21:45:13 +0800

    added cleaning for eda

--- run.sh
+++ run.sh
@@ -6,16 +6,19 @@ set -e
 echo "====================================="
 echo "Starting Kedro pipeline execution..."
 echo "====================================="
-python -m kedro run --pipeline "dataprep"
+# python -m kedro run --pipeline "dataprep"
+kedro run --pipeline "dataprep"
 
 echo "====================================="
 echo "Training the model..."
 echo "====================================="
-python -m kedro run --pipeline "model"
+# python -m kedro run --pipeline "model"
+kedro run --pipeline "model"
 
 echo "====================================="
 echo "Evaluating the model..."
 echo "====================================="
-python -m kedro run --pipeline "evaluation"
+# python -m kedro run --pipeline "evaluation"
+kedro run --pipeline "model_report"
 
 echo "All steps completed successfully!"
